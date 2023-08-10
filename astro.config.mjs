3e7a5212f36c3a0ba59846167d1405919f5d648e
Akayy <techakayy@gmail.com>
2023-08-10 13:38:11 +1000

    Added "unplugin-auto-import"

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import svelte from '@astrojs/svelte'
 import Pinegrow from '@pinegrow/astro-module'
 import { fileURLToPath, URL } from 'node:url'
 import AutoImportComponents from 'unplugin-vue-components/vite'
+import AutoImportAPIs from 'unplugin-auto-import/astro'
 import Unocss from 'unocss/astro'
 import presetIcons from '@unocss/preset-icons'
 // import VueDevTools from 'vite-plugin-vue-devtools'
@@ -47,6 +48,30 @@ export default defineConfig({
     }),
     mdx(),
     sitemap(),
+    // For details, refer to https://github.com/antfu/unplugin-auto-import#configuration
+    AutoImportAPIs({
+      include: [
+        /.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /.vue$/,
+        /.vue?vue/, // .vue
+        /.md$/, // .md
+      ],
+      imports: [
+        'vue',
+        // 'vue-router',
+        // 'vue-i18n',
+        // 'vue/macros',
+        // '@vueuse/head',
+        // '@vueuse/core',
+        // 'pinia',
+      ],
+      dirs: [
+        // 'src/composables',
+        // 'src/stores',
+      ],
+      vueTemplate: true,
+      dts: 'auto-imports.d.ts',
+    }),
   ],
   vite: {
     plugins: [
