e5ebc8e8c13e310f4a6e001de7d73894b8343379
Akayy <techakayy@gmail.com>
2023-10-31 21:08:00 +1100

    Centered

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -2,6 +2,19 @@
   //...
 </script>
 <template>
-  <div><span>A Fresh Start!</span></div>
+  <div>
+    <div
+      style="
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        padding-top: 1.5rem /* 24px */;
+        padding-bottom: 1.5rem /* 24px */;
+      "
+    >
+      <span>A Fresh Start!</span>
+    </div>
+  </div>
 </template>
 <style scoped></style>
