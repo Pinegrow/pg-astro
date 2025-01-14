4a167f3eb3751d9a0f39971804b9c4e4f388c7ee
Akayy <techakayy@gmail.com>
2025-01-14 12:17:25 +1100

    Added root hydrated PageHome

---
+++ src/components/PageHome.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  import { heroImageUrl } from '@/utils/hero'
+</script>
+<template>
+  <div>
+    <div
+      style="
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        padding-top: 1.5rem /* 24px */;
+        padding-bottom: 1.5rem /* 24px */;
+      "
+    >
+      <slot /><span style="margin-left: 0.5rem; /* 8px */">A Fresh Start!</span>
+    </div>
+  </div>
+</template>
+<style></style>
